421e722d0f91b31b5be7bcb90062d95d963a4241
Quentin Roux <quentin.roux@hotmail.fr>
2021-01-20 20:50:09 +0100

    email provider setup

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'c8148307aa9411a93f035592000c5ef2'),
+      secret: env("ADMIN_JWT_SECRET", "c8148307aa9411a93f035592000c5ef2"),
     },
   },
 });
