8d4b8c8321f3dec12b69771bcaf526aaf90b3559
Quentin Roux <quentin.roux@hotmail.fr>
2021-01-20 21:09:23 +0100

    env setup

--- config/plugins.js
+++ config/plugins.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   email: {
     provider: "sendgrid",
     providerOptions: {
-      apiKey: env("SENDGRID_API_KEY"),
+      apiKey: process.env.SENDGRID_API_KEY || env("SENDGRID_API_KEY"),
     },
     settings: {
       defaultFrom: "quentin.roux@hotmail.fr",
